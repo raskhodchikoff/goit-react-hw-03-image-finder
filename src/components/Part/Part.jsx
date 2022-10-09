47038ef8fe7a0bec8d09ed0f410f62f860998e39
raskhodchikoff <artlan@ukr.net>
2022-10-09 21:48:07 +0300

    init

--- src/components/Part/Part.jsx
+++
@@ -1,9 +0,0 @@
-// import { PropTypes } from 'prop-types';
-import { Section } from 'components/Part/Part.styled';
-import { Box } from './../Box';
-
-export const Filter = () => (
-  <Box as="section">
-    <Section />
-  </Box>
-);
