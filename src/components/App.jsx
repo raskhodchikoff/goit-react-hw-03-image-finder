b70b943fbfecc7a7f3edb86c6372f39592ff715c
raskhodchikoff <artlan@ukr.net>
2022-10-09 22:08:53 +0300

    init

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+// import { PropTypes } from 'prop-types';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import { nanoid } from 'nanoid';
+// import { Formik, ErrorMessage } from 'formik';
+// import * as yup from 'yup';
+
+import { Part } from 'components/Part/Part';
+
+import { Example } from './App.styled';
+import { Box } from './Box';
+
+export class App extends Component {
+  render() {
+    return (
+      <Box as="main" mx="auto">
+        <Example />
+        <Part />
+      </Box>
+    );
+  }
+}
